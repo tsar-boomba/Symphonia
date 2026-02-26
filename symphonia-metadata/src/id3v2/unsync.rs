9bf7bcfb1e20dbb29fabf03c96d1f5848fbbd7ea
tsar-boomba <itg.2048@gmail.com>
2026-02-25 20:00:48 -0500

    remove unused uses in unsync reader

--- symphonia-metadata/src/id3v2/unsync.rs
+++ symphonia-metadata/src/id3v2/unsync.rs
@@ -5,8 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use alloc::boxed::Box;
-use symphonia_core::async_trait;
 use symphonia_core::errors::Result;
 use symphonia_core::io::{self, FiniteStream, ReadBytes};
 
